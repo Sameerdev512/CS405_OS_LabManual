d959d3b737c2ea7b3ed0a08cc1e41b7e5ab2ba9e
Sameerdev512 <156842673+Sameerdev512@users.noreply.github.com>
2024-05-27 22:48:00 +0530

    Add files via upload

--- program10.c
+++ program10.c
@@ -1,168 +1,299 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define MAX_FRAMES 
-#define MAX_PAGES 
-
-int pages[MAX_PAGES];
-int pageFrames[MAX_FRAMES];
-int total_pages, frames;
-
-// Prototypes
-void FIFO();
-void LRU();
-void OPTIMAL();
-int findPage(int page, int pageFrames[], int frames);
-
-int findPage(int page, int pageFrames[], int frames)
-{
-    for (int i = 0; i < frames; i++)
-    {
-        if (pageFrames[i] == page)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void initialize()
-{
-    for (int i = 0; i < frames; i++)
-    {
-        pageFrames[i] = -1; // initialize page frames to -1 indicating they're empty
-    }
-}
-
-void printPageFrames()
-{
-    for (int i = 0; i < frames; i++)
-    {
-        if (pageFrames[i] != -1)
-        {
-            printf("%d ", pageFrames[i]);
-        }
-        else
-        {
-            printf("-- ");
-        }
-    }
-    printf("\n");
-}
-
-void FIFO()
-{
-    int next_replace = 0;
-    int page_faults = 0;
-
-    initialize();
-
-    for (int i = 0; i < total_pages; i++)
-    {
-        if (findPage(pages[i], pageFrames, frames) == -1)
-        {
-            pageFrames[next_replace] = pages[i];
-            next_replace = (next_replace + 1) % frames;
-            page_faults++;
-            printPageFrames();
-        }
-    }
-    printf("FIFO - Total Page Faults: %d\n", page_faults);
-}
-
-void LRU()
-{
-    int page_faults = 0;
-    int used[MAX_FRAMES] = {0}; // Used to keep track of usage
-
-    initialize();
-
-    for (int i = 0; i < total_pages; i++)
-    {
-        int found = findPage(pages[i], pageFrames, frames);
-        if (found == -1)
-        { // Page fault
-            int lru = 0;
-            for (int j = 1; j < frames; j++)
-            {
-                if (used[j] < used[lru])
-                {
-                    lru = j;
-                }
-            }
-            pageFrames[lru] = pages[i];
-            page_faults++;
-            printPageFrames();
-        }
-        for (int k = 0; k < frames; k++)
-        {
-            used[k]++; // Increment the use time of all frames
-        }
-        if (found != -1)
-        {
-            used[found] = 0; // Reset the frame that was just referenced
-        }
-    }
-    printf("LRU - Total Page Faults: %d\n", page_faults);
-}
-
-void OPTIMAL()
-{
-    int page_faults = 0;
-
-    initialize();
-
-    for (int i = 0; i < total_pages; i++)
-    {
-        if (findPage(pages[i], pageFrames, frames) == -1)
-        { // Page fault
-            int opt_index = -1, farthest = i;
-            for (int j = 0; j < frames; j++)
-            {
-                int k;
-                for (k = i + 1; k < total_pages; k++)
-                {
-                    if (pageFrames[j] == pages[k])
-                    {
-                        if (k > farthest)
-                        {
-                            farthest = k;
-                            opt_index = j;
-                        }
-                        break;
-                    }
-                }
-                if (k == total_pages)
-                { // Not found in the future
-                    opt_index = j;
-                    break;
-                }
-            }
-            if (opt_index == -1)
-            {
-                opt_index = 0; // If all are equally likely
-            }
-            pageFrames[opt_index] = pages[i];
-            page_faults++;
-            printPageFrames();
-        }
-    }
-    printf("Optimal - Total Page Faults: %d\n", page_faults);
-}
+void fcfs(int requests[], int size, int head);
+void sstf(int requests[], int size, int head);
+void scan(int requests[], int size, int head);
+void cscan(int requests[], int size, int head);
+void look(int requests[], int size, int head);
+void clook(int requests[], int size, int head);
+void sort(int arr[], int n);
+int absolute(int x);
 
 int main()
 {
-    printf("Enter number of frames: ");
-    scanf("%d", &frames);
-    printf("Enter number of pages: ");
-    scanf("%d", &total_pages);
-    printf("Enter page reference string:\n");
-    for (int i = 0; i < total_pages; i++)
-    {
-        scanf("%d", &pages[i]);
-    }
-    FIFO();
-    LRU();
-    OPTIMAL();
+    int requests[] = {98, 183, 37, 122, 14, 124, 65, 67};
+    int head = 53;
+    int size = sizeof(requests) / sizeof(requests[0]);
+
+    printf("FCFS:\n");
+    fcfs(requests, size, head);
+
+    printf("\nSSTF:\n");
+    sstf(requests, size, head);
+
+    printf("\nSCAN:\n");
+    scan(requests, size, head);
+
+    printf("\nC-SCAN:\n");
+    cscan(requests, size, head);
+
+    printf("\nLOOK:\n");
+    look(requests, size, head);
+
+    printf("\nC-LOOK:\n");
+    clook(requests, size, head);
+
     return 0;
+}
+
+void fcfs(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+
+    for (int i = 0; i < size; i++)
+    {
+        curTrack = requests[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+}
+
+void sstf(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+    int *left = (int *)malloc(size * sizeof(int));
+    int *right = (int *)malloc(size * sizeof(int));
+    int lsize = 0, rsize = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (requests[i] < head)
+            left[lsize++] = requests[i];
+        else if (requests[i] > head)
+            right[rsize++] = requests[i];
+    }
+
+    sort(left, lsize);
+    sort(right, rsize);
+
+    while (lsize > 0 || rsize > 0)
+    {
+        if (lsize > 0 && rsize > 0)
+        {
+            if (absolute(left[lsize - 1] - head) < absolute(right[0] - head))
+            {
+                curTrack = left[--lsize];
+            }
+            else
+            {
+                curTrack = right[0];
+                for (int i = 1; i < rsize; i++)
+                    right[i - 1] = right[i];
+                rsize--;
+            }
+        }
+        else if (lsize > 0)
+        {
+            curTrack = left[--lsize];
+        }
+        else
+        {
+            curTrack = right[0];
+            for (int i = 1; i < rsize; i++)
+                right[i - 1] = right[i];
+            rsize--;
+        }
+
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+    free(left);
+    free(right);
+}
+
+void scan(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+    int *left = (int *)malloc((size + 1) * sizeof(int));
+    int *right = (int *)malloc(size * sizeof(int));
+    int lsize = 0, rsize = 0;
+
+    left[lsize++] = 0; // Add start
+    for (int i = 0; i < size; i++)
+    {
+        if (requests[i] < head)
+            left[lsize++] = requests[i];
+        else
+            right[rsize++] = requests[i];
+    }
+
+    sort(left, lsize);
+    sort(right, rsize);
+
+    for (int i = lsize - 1; i >= 0; i--)
+    {
+        curTrack = left[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    for (int i = 0; i < rsize; i++)
+    {
+        curTrack = right[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+    free(left);
+    free(right);
+}
+
+void cscan(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+    int *left = (int *)malloc((size + 1) * sizeof(int));
+    int *right = (int *)malloc((size + 1) * sizeof(int));
+    int lsize = 0, rsize = 0;
+
+    left[lsize++] = 0;    // Add start
+    right[rsize++] = 199; // Add end
+
+    for (int i = 0; i < size; i++)
+    {
+        if (requests[i] < head)
+            left[lsize++] = requests[i];
+        else
+            right[rsize++] = requests[i];
+    }
+
+    sort(left, lsize);
+    sort(right, rsize);
+
+    for (int i = 0; i < rsize; i++)
+    {
+        curTrack = right[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    head = 0; // Move head to the start of the disk
+
+    for (int i = 0; i < lsize; i++)
+    {
+        curTrack = left[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+    free(left);
+    free(right);
+}
+
+void look(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+    int *left = (int *)malloc(size * sizeof(int));
+    int *right = (int *)malloc(size * sizeof(int));
+    int lsize = 0, rsize = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (requests[i] < head)
+            left[lsize++] = requests[i];
+        else
+            right[rsize++] = requests[i];
+    }
+
+    sort(left, lsize);
+    sort(right, rsize);
+
+    for (int i = lsize - 1; i >= 0; i--)
+    {
+        curTrack = left[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    for (int i = 0; i < rsize; i++)
+    {
+        curTrack = right[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+    free(left);
+    free(right);
+}
+
+void clook(int requests[], int size, int head)
+{
+    int seekCount = 0;
+    int distance, curTrack;
+    int *left = (int *)malloc(size * sizeof(int));
+    int *right = (int *)malloc(size * sizeof(int));
+    int lsize = 0, rsize = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (requests[i] < head)
+            left[lsize++] = requests[i];
+        else
+            right[rsize++] = requests[i];
+    }
+
+    sort(left, lsize);
+    sort(right, rsize);
+
+    for (int i = 0; i < rsize; i++)
+    {
+        curTrack = right[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    for (int i = 0; i < lsize; i++)
+    {
+        curTrack = left[i];
+        distance = absolute(curTrack - head);
+        seekCount += distance;
+        head = curTrack;
+    }
+
+    printf("Total seek operations: %d\n", seekCount);
+    free(left);
+    free(right);
+}
+
+void sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int absolute(int x)
+{
+    return x > 0 ? x : -x;
 }
